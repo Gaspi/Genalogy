ee64157c7642ff06327fc5e81cdc66366fff0127
Gaspard FEREY <gaspard.ferey@hotmail.fr>
2015-04-30 08:33:30 -0400

    My first class!

--- index.php
+++ index.php
@@ -4,14 +4,27 @@
 
 <h1>My first SVG</h1>
 
-<svg width="100" height="100">
+<svg width="600" height="300">
 <?php
 
 
+class Node {
+   private $name;
+
+   function __construct($name) {
+      $this->$name = $name;
+   }
+
+   public function __toString() {
+      return "<rect x=\"50\" y=\"20\" rx=\"20\" ry=\"20\" width=\"150\" height=\"150\" style=\"fill:green;fille-opacity:0.1;stroke:black;stroke-width:5;stroke-opacity:0.9\"/>";
+   }
+}
 
 
 
 
+$node = new Node("Test");
+echo $node;
 
 ?>
 </svg>
